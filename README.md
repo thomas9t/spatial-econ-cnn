f63aed5b10a4366fcfd62e28986743c67000eab5
Anthony Thomas <ahthomas@eng.ucsd.edu>
2021-04-18 12:14:15 -0700

    Adds scripts and run script

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# spatial-econ-cnn
+# Using Neural Networks to Predict Micro-Spatial Economic Growth
 Repository of code associated with the paper "Using Neural Networks to Predict Micro-Spatial Economic Growth"
+
+This code was run on a Linux computer using `Python 3.6–3.8, pip and venv >= 19.0, tensorflow >= 2.0, numpy >= 1.2`
+
+# Instructions
+1. Download data ([GoogleDrive](https://drive.google.com/drive/folders/1MnyQddPAzGWjZrHXlErNDfbJv9dMhr_I?usp=sharing))
+2. Un-compress `tar -xvf ...` and move into `data/` directory
+3. Download pretrained model weights ([GoogleDrive](https://drive.google.com/drive/folders/1n8znM2_A3Q6RLhVRxgU1HBW8FG_g6ndY?usp=sharing))
+4. Un-compress `tar -xvf ..` and move into `weights/` directory
+5. Run the script `run.sh`. The script will create a virtual-environment, install dependencies and run scripts for training. There are several different run configurations listed in `run.sh` which can reproduce various aspects of the paper (e.g. `RGB only` models or models with nighlights). Inspect `run.sh` for more detail.
---
+++ run.sh
@@ -0,0 +1,83 @@
+# download data and pretrained weights
+# data: https://drive.google.com/drive/folders/1MnyQddPAzGWjZrHXlErNDfbJv9dMhr_I?usp=sharing
+# weights: https://drive.google.com/drive/folders/1n8znM2_A3Q6RLhVRxgU1HBW8FG_g6ndY?usp=sharing
+# put data files into "data/"
+# put weights files into "weights/"
+
+# require Python 3.6–3.8, pip and venv >= 19.0, tensorflow >= 2.0, numpy >= 1.2
+python3 -m venv --system-site-packages ./scnn
+source ./scnn/bin/activate
+pip install --upgrade pip
+pip install --upgrade tensorflow numpy
+
+
+# train level model
+# mw data only have RGB channels for both high and low resolution
+# small images don't have nightlight channel
+# True: with initial conditions, False: without initial conditions
+# Arguments:
+# construct: Level of analysis. May be: BG or block
+# region: Region of analysis. May be: 'national' or 'mw' (for midwest)
+# model_type: What channels should be used. May be: 'base', 'RGB' (for RGB only), 'nl' (for base + nighlights)
+# size: What size of images should be used. May be:'large' (for 2.4 km^2) 'small' (for 1.2 km^2)
+# datatype: What outcome variable should be predicted. May be: 'inc' or 'pop'
+# resolution: What data resolution should be used. May be: 'high' or 'low'
+# with_feature: Should initial conditions be included. May be: True, False
+# epochs: How many epochs should be used for training? May be: positive integer
+# data_dir: Where is the data. May be: string path
+# out_dir: Where should output be written? May be: string path
+
+python scripts/train_level_model.py block national base large inc low True 200 data outputs
+# python scripts/train_level_model.py block national base large pop low True 200 data outputs
+# python scripts/train_level_model.py block national RGB large inc low True 200 data outputs
+# python scripts/train_level_model.py block national RGB large pop low True 200 data outputs
+# python scripts/train_level_model.py block national nl large inc low True 200 data outputs
+# python scripts/train_level_model.py block national nl large pop low True 200 data outputs
+# python scripts/train_level_model.py block national base small inc low True 200 data outputs
+# python scripts/train_level_model.py block national base small pop low True 200 data outputs
+# python scripts/train_level_model.py block national RGB small inc low True 200 data outputs
+# python scripts/train_level_model.py block national RGB small pop low True 200 data outputs
+# python scripts/train_level_model.py block mw RGB small inc low True 200 data outputs
+# python scripts/train_level_model.py block mw RGB small pop low True 200 data outputs
+# python scripts/train_level_model.py block mw RGB small inc high True 200 data outputs
+# python scripts/train_level_model.py block mw RGB small pop high True 200 data outputs
+# python scripts/train_level_model.py block national base large inc low False 200 data outputs
+# python scripts/train_level_model.py block national base large pop low False 200 data outputs
+# python scripts/train_level_model.py block national RGB large inc low False 200 data outputs
+# python scripts/train_level_model.py block national RGB large pop low False 200 data outputs
+# python scripts/train_level_model.py block national nl large inc low False 200 data outputs
+# python scripts/train_level_model.py block national nl large pop low False 200 data outputs
+# python scripts/train_level_model.py block national base small inc low False 200 data outputs
+# python scripts/train_level_model.py block national base small pop low False 200 data outputs
+# python scripts/train_level_model.py block national RGB small inc low False 200 data outputs
+# python scripts/train_level_model.py block national RGB small pop low False 200 data outputs
+# python scripts/train_level_model.py block mw RGB small inc low False 200 data outputs
+# python scripts/train_level_model.py block mw RGB small pop low False 200 data outputs
+# python scripts/train_level_model.py block mw RGB small inc high False 200 data outputs
+# python scripts/train_level_model.py block mw RGB small pop high False 200 data outputs
+
+# train difference model
+# Arguments:
+# construct: Level of analysis. May be: BG or block
+# region: Region of analysis. May be: 'national' or 'mw' (for midwest)
+# model_type: What channels should be used. May be: 'base', 'RGB' (for RGB only), 'nl' (for base + nighlights)
+# size: What size of images should be used. May be:'large' (for 2.4 km^2) 'small' (for 1.2 km^2)
+# datatype: What outcome variable should be predicted. May be: 'inc' or 'pop'
+# resolution: What data resolution should be used. May be: 'high' or 'low'
+# with_feature: Should initial conditions be included. May be: True, False
+# epochs: How many epochs should be used for training? May be: positive integer
+# data_dir: Where is the data. May be: string path
+# out_dir: Where should output be written? May be: string path
+# weight_dir: Where are weights (produced by training levels models)? May be: string path
+# Levels models hyperparameters (each is a separate argument):
+    # level_lr
+    # level_l2
+    # level_bs
+    # level_ds
+    # level_nf
+    # level_dr
+    # level_epochs
+python scripts/train_diff_model.py block national base large inc low True 100 <dirname> outputs weights 1e-4 1e-8 16 200 32 0.5 200
+# python scripts/train_diff_model.py block national base large pop low True 100 <dirname> outputs weights 1e-4 1e-7 16 50 32 0.5 200
+# python scripts/train_diff_model.py block national base small inc low True 100 <dirname> outputs weights 1e-4 1e-8 16 200 32 0.5 200
+# python scripts/train_diff_model.py block national base small pop low True 100 <dirname> outputs weights 1e-4 1e-7 16 50 32 0.5 200
