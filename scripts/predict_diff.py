25f4d42eee38ff2f8e98706e69c3645cade085e6
wdzhy123 <h1zhou@ucsd.edu>
2021-12-19 16:40:25 -0800

    Add scripts for generating predictions

--- scripts/predict_diff.py
+++ scripts/predict_diff.py
@@ -27,56 +27,60 @@ ds = int(sys.argv[14])
 nf = int(sys.argv[15])
 dr = float(sys.argv[16])
 epochs = int(sys.argv[17])
+sep = int(sys.argv[18])
 test_type = 'prediction'
 
 if (region != 'national') | (model_type != 'base'):
         sys.exit('Currently prediction only works for national base model')
 
-weight_dir = '{}/{}_{}_national_level_base{}_{}_{}/checkpoints/{}_{}_{}_{}_{}_{}' \
-        .format(weight_dir, construct, size,'_feature' if with_feature else '', datatype, epochs, lr, l2, bs, ds, nf, dr)
 ds_dir = '{}/{}_{}_{}_{}_sharded/{}_{}_{}_{}_{}_*-of-*.tfrecords' \
         .format(data_dir, size, construct, test_type, region, '*', construct, size, test_type, region)
 
+weight_dir = '{}/{}_{}_{}_diff_{}{}_{}_{}/checkpoints/{}_{}_{}_{}_{}_{}' \
+        .format(weight_dir, construct, size, region, model_type, '_feature' if with_feature else '', datatype, epochs, lr, l2, bs, ds, nf, dr)
+
 def main():
-    columns = ['{}_{}'.format(datatype, year)for year in range(20)]
+    columns = ['{}_{}_{}'.format(datatype, year, year+sep) for year in range(20-sep)]
     columns = ['img_id'] + columns
     df = pd.DataFrame(columns=columns)
     img_size, _, _, n_bands, _ = get_img_size(size, model_type, region, resolution)
     model = make_level_model(img_size, n_bands, l2, nf, dr, with_feature)
-    model.load_weights(weight_dir).expect_partial()
-    ds = read_files(ds_dir, lambda x: parse(x, get_feature_description(test_type)))
-    df = predict(ds, model, df)
-    df.to_csv('{}/{}_{}_{}_level_{}{}_{}_all_years_predictions.csv'.format(out_dir, construct, size, region, model_type, '_feature' if with_feature else '', datatype), index=False)
+    diff_model = make_diff_model(img_size, n_bands, l2, nf, dr, with_feature, model)
+    diff_model.load_weights(weight_dir).expect_partial()
+    ds = read_files(ds_dir.format(ds_dir), lambda x: parse(x, get_feature_description(test_type)))
+    df = predict(ds, diff_model, df)
+    df.to_csv('{}/{}_{}_{}_diff_{}{}_{}_{}_years_predictions.csv'.format(out_dir, construct, size, region, model_type, '_feature' if with_feature else '', datatype, sep), index=False)
     print('complete!')
 
 
-def predict(ds, model, df):
+def predict(ds, diff_model, df):
     if with_feature:
-        for img, features, img_id in tqdm(ds.as_numpy_iterator()):
-            predictions = model((img, features), training=False)
+        for img0, img1, features, img_id in tqdm(ds.as_numpy_iterator()):
+            predictions = diff_model((img0, img1, features), training=False)
             row = {'img_id': img_id}
-            row.update({'{}_{}'.format(datatype, year): predictions[year].numpy()[0] for year in range(20)})
+            row.update({'{}_{}_{}'.format(datatype, year, year+sep): predictions[year].numpy()[0] for year in range(20-sep)})
             df = df.append(row, ignore_index=True)
     else:
-        for img, img_id in tqdm(ds.as_numpy_iterator()):
-            predictions = model(img, training=False)
+        for img0, img1, img_id in tqdm(ds.as_numpy_iterator()):
+            predictions = diff_model((img0, img1), training=False)
             row = {'img_id': img_id}
-            row.update({'{}_{}'.format(datatype, year): predictions[year].numpy()[0] for year in range(20)})
+            row.update({'{}_{}_{}'.format(datatype, year, year+sep): predictions[year].numpy()[0] for year in range(20-sep)})
             df = df.append(row, ignore_index=True)
     return df
 
 
 def parse(serialized_example, feature_description):
     example = tf.io.parse_single_example(serialized_example, feature_description)
-    img = tf.stack([tf.clip_by_value(tf.io.parse_tensor(example['image{}'.format(y)], out_type=float)[:, :, 0:7], 0, 1) for y in range(20)], 0)
+    img0 = tf.stack([tf.clip_by_value(tf.io.parse_tensor(example['image{}'.format(y)], out_type=float)[:, :, 0:7], 0, 1) for y in range(20-sep)], 0)
+    img1 = tf.stack([tf.clip_by_value(tf.io.parse_tensor(example['image{}'.format(y+sep)], out_type=float)[:, :, 0:7], 0, 1) for y in range(20-sep)], 0)
     img_id = example['img_id']
     if with_feature:
         features = tf.io.parse_tensor(example['baseline_features'], out_type=float)
         features = tf.reshape(features, (34,))
-        features = tf.stack([features for y in range(20)], 0)
-        return img, features, img_id
+        features = tf.stack([features for y in range(20-sep)], 0)
+        return img0, img1, features, img_id
     else:
-        return img, img_id
+        return img0, img1, img_id
 
 
 if __name__ == "__main__":
